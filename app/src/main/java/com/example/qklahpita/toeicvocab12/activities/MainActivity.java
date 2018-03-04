efd5bcc50d106ea1b8be65011578ac26595b1ce6
Qk Lahpita <techkids.a12@gmail.com>
2018-03-04 15:59:25 +0700

    Session 8 part 2: Setup expandable list view

--- app/src/main/java/com/example/qklahpita/toeicvocab12/activities/MainActivity.java
+++ app/src/main/java/com/example/qklahpita/toeicvocab12/activities/MainActivity.java
@@ -2,17 +2,37 @@ package com.example.qklahpita.toeicvocab12.activities;
 
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.widget.ExpandableListView;
 
-import com.example.qklahpita.toeicvocab12.databases.DatabaseManager;
 import com.example.qklahpita.toeicvocab12.R;
+import com.example.qklahpita.toeicvocab12.adapters.ToeicExpandableListViewAdapter;
+import com.example.qklahpita.toeicvocab12.databases.DatabaseManager;
+import com.example.qklahpita.toeicvocab12.databases.models.CategoryModel;
+import com.example.qklahpita.toeicvocab12.databases.models.TopicModel;
+
+import java.util.HashMap;
+import java.util.List;
 
 public class MainActivity extends AppCompatActivity {
+
+    ExpandableListView elvToeic;
+    ToeicExpandableListViewAdapter toeicExpandableListViewAdapter;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
 
-        DatabaseManager.getInstance(this).getListTopic();
+        elvToeic = findViewById(R.id.elv_toeic);
+
+        List<TopicModel> topicModelList = DatabaseManager.getInstance(this).getListTopic();
+        List<CategoryModel> categoryModelList = DatabaseManager.getInstance(this)
+                .getListCategory(topicModelList);
+        HashMap<String, List<TopicModel>> hashMap = DatabaseManager.getInstance(this).getHashMapTopic(
+                topicModelList, categoryModelList);
+
+        toeicExpandableListViewAdapter = new ToeicExpandableListViewAdapter(categoryModelList, hashMap);
+
+        elvToeic.setAdapter(toeicExpandableListViewAdapter);
     }
 }
--- app/src/main/java/com/example/qklahpita/toeicvocab12/adapters/ToeicExpandableListViewAdapter.java
+++ app/src/main/java/com/example/qklahpita/toeicvocab12/adapters/ToeicExpandableListViewAdapter.java
@@ -1,9 +1,16 @@
 package com.example.qklahpita.toeicvocab12.adapters;
 
+import android.graphics.Color;
+import android.support.v7.widget.CardView;
+import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
 import android.widget.BaseExpandableListAdapter;
+import android.widget.ImageView;
+import android.widget.ProgressBar;
+import android.widget.TextView;
 
+import com.example.qklahpita.toeicvocab12.R;
 import com.example.qklahpita.toeicvocab12.databases.models.CategoryModel;
 import com.example.qklahpita.toeicvocab12.databases.models.TopicModel;
 
@@ -26,32 +33,34 @@ public class ToeicExpandableListViewAdapter extends BaseExpandableListAdapter {
 
     @Override
     public int getGroupCount() {
-        return 0;
+        return categoryModelList.size();
     }
 
+    //index cua children trong group
     @Override
     public int getChildrenCount(int i) {
-        return 0;
+        return topicModelHashMap.get(categoryModelList.get(i).name).size();
     }
 
     @Override
     public Object getGroup(int i) {
-        return null;
+        return categoryModelList.get(i);
     }
 
+    //i: index group, i1: index children
     @Override
     public Object getChild(int i, int i1) {
-        return null;
+        return topicModelHashMap.get(categoryModelList.get(i).name).get(i1);
     }
 
     @Override
     public long getGroupId(int i) {
-        return 0;
+        return i;
     }
 
     @Override
     public long getChildId(int i, int i1) {
-        return 0;
+        return i1;
     }
 
     @Override
@@ -60,17 +69,59 @@ public class ToeicExpandableListViewAdapter extends BaseExpandableListAdapter {
     }
 
     @Override
-    public View getGroupView(int i, boolean b, View view, ViewGroup viewGroup) {
-        return null;
+    public View getGroupView(int i, boolean isExpanded, View view, ViewGroup viewGroup) {
+        LayoutInflater layoutInflater = LayoutInflater.from(viewGroup.getContext());
+        view = layoutInflater.inflate(R.layout.item_list_category, viewGroup, false);
+
+        CategoryModel categoryModel = (CategoryModel) getGroup(i);
+        TextView tvCategory = view.findViewById(R.id.tv_category);
+        TextView tvCategoryDes = view.findViewById(R.id.tv_category_des);
+        ImageView ivArrow = view.findViewById(R.id.iv_arrow);
+        CardView cvCategory = view.findViewById(R.id.cv_category);
+
+        cvCategory.setCardBackgroundColor(Color.parseColor(categoryModel.color));
+
+        if (isExpanded) {
+            ivArrow.setImageResource(R.drawable.ic_keyboard_arrow_up_black_24dp);
+        } else {
+            ivArrow.setImageResource(R.drawable.ic_keyboard_arrow_down_black_24dp);
+        }
+
+        tvCategory.setText(categoryModel.name);
+
+        String des = "";
+        for (int j = 0; j < 5; j++) {
+            des += topicModelHashMap.get(categoryModel.name).get(j).name;
+            if (j != 4) {
+                des += ", ";
+            }
+        }
+        tvCategoryDes.setText(des);
+
+        return view;
     }
 
     @Override
     public View getChildView(int i, int i1, boolean b, View view, ViewGroup viewGroup) {
-        return null;
+        LayoutInflater layoutInflater = LayoutInflater.from(viewGroup.getContext());
+        view = layoutInflater.inflate(R.layout.item_list_topic, viewGroup, false);
+
+        TopicModel topicModel = (TopicModel) getChild(i, i1);
+
+        TextView tvTopic = view.findViewById(R.id.tv_name_topic);
+        TextView tvLastTime = view.findViewById(R.id.tv_last_time);
+        ProgressBar pbTopic = view.findViewById(R.id.pb_topic);
+
+        tvTopic.setText(topicModel.name);
+        if (topicModel.lastTime != null) {
+            tvLastTime.setText(topicModel.lastTime);
+        }
+
+        return view;
     }
 
     @Override
     public boolean isChildSelectable(int i, int i1) {
-        return false;
+        return true;
     }
 }
