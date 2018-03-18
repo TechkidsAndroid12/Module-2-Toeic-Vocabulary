7ace48c5fc61d2202d2899632673b187e3636e5f
Qk Lahpita <techkids.a12@gmail.com>
2018-03-18 10:27:23 +0700

    Homework Session 10: Setting Screen UI

--- app/src/main/java/com/example/qklahpita/toeicvocab12/activities/MainActivity.java
+++ app/src/main/java/com/example/qklahpita/toeicvocab12/activities/MainActivity.java
@@ -3,6 +3,9 @@ package com.example.qklahpita.toeicvocab12.activities;
 import android.content.Intent;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.Menu;
+import android.view.MenuInflater;
+import android.view.MenuItem;
 import android.view.View;
 import android.widget.ExpandableListView;
 
@@ -62,5 +65,22 @@ public class MainActivity extends AppCompatActivity {
     protected void onStart() {
         super.onStart();
         toeicExpandableListViewAdapter.refreshList(this);
+    }
+
+    @Override
+    public boolean onCreateOptionsMenu(Menu menu) {
+        MenuInflater inflater = getMenuInflater();
+        inflater.inflate(R.menu.menu_setting, menu);
+
+        // return true so that the menu pop up is opened
+        return true;
+    }
+
+    @Override
+    public boolean onOptionsItemSelected(MenuItem item) {
+        Intent intent = new Intent(this, SettingActivity.class);
+        startActivity(intent);
+
+        return super.onOptionsItemSelected(item);
     }
 }
